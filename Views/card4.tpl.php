28fbecd1426281b33d682cf51d2b431273edf2bd
Deunoh <denovann@live.fr>
2024-02-25 18:01:58 +0100

    ajout fonction mail et style

--- Views/card4.tpl.php
+++ Views/card4.tpl.php
@@ -1,6 +1,7 @@
 <?php
 $errorList = [];
-
+$textContent1 = 'Vous avez besoin d’obtenir d’autres informations ou de me rencontrer ?';
+$textContent2 = ' Remplissez ce formulaire !';
 if(!empty($_POST)){
     $name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
     $firstname = isset($_POST['firstname']) ? htmlspecialchars($_POST['firstname']) : '';
@@ -30,10 +31,40 @@ if(!empty($_POST)){
     // Si aucune erreur n'est détectée, traiter le formulaire (envoi d'email, etc.)
     if(empty($errorList)) {
 
-        //TODO A SUPPRIMER !!!
-       var_dump('envoi du mail');
-       
-    }
+                // Envoi de l'e-mail
+        $to = "denovann@live.fr";
+        $subject = "Nouveau message du formulaire de contact";
+        $messageBody = "Nom: " . utf8_encode($name) . "\n";
+        $messageBody .= "Prenom: $firstname\n";
+        $messageBody .= "Email: $email\n";
+        $messageBody .= "Message:\n$message";
+        $headers = "Content-Type: text/plain; charset=utf-8";
+        // Envoi du mail
+        $success = mail($to, $subject, $messageBody, $headers);
+
+        if (filter_has_var(INPUT_POST, 'copy')) {
+        $to = $email;
+        $subject = "Votre message pour Denovann";
+        $messageBody = "Voici le message que vous m'avez envoyé : " . $message;
+        $headers = "From: denovann@live.fr\r\n";
+        $headers .= "Reply-To: denovann@live.fr\r\n";
+        $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+
+        $successExped = mail($to, $subject, $messageBody, $headers);
+        }
+
+        if($success){
+            $textContent1 = "Votre message a été envoyé avec succès.";
+            $textContent2 = "";
+            header("Location: " . $_SERVER['PHP_SELF'] . "#quatre");
+            exit();
+        } else {
+            $textContent1 = "Il y a eu une erreur lors de l'envoi de votre message.";
+            $textContent2 = "";
+            $styleAlert = true;
+        };
+            
+            }
 }
 
 ?>
@@ -55,32 +86,32 @@ function showErrors($fieldName, $errorList = null) {
                     <div class="label-fixed">
                         <h3>Contact</h3>
                     </div>
-                    <!-- TODO Faire une variable PHP en cas de sucess et remplacer le texte de contact-text -->
+                   
                     <section class="mid-section">
                         <h2 class="title-card">Contact<span id="input-cursor">|</span></h2>
-                       <p class="contact-text">Vous avez besoin d’obtenir d’autres informations ou de me rencontrer ?</p>
-                       <p class="contact-text"> Remplissez ce formulaire !</p>
+                        <p class="contact-text <?= isset($styleAlert) && $styleAlert === true ? 'red_text' : '' ?>"><?= $textContent1 ?></p>
+                       <p class="contact-text"><?= $textContent2 ?></p>
                     </section>
 
                     <section class="formulaire-section">
                         <div class="formulaire-box">
                             <form method="post">
-                              <span class="champs_obligatoires"><span class="red_star">*</span> champs obligatoires</span>
+                              <span class="champs_obligatoires"><span class="yellow_star">*</span> champs obligatoires</span>
                                     <div class="inputbox <?= (isset($errorList['name'])) ? 'invalid' : ''; ?>">
                                     <input type="text" name="name" <?php if (!empty($errorList)) { echo 'placeholder="' . showErrors('name', $errorList) . '"';} ?> required>
-                                        <label for="nom">Nom<span class="red_star">*</span></label>
+                                        <label for="nom">Nom<span class="yellow_star">*</span></label>
                                     </div>
                                     <div class="inputbox <?= (isset($errorList['firstname'])) ? 'invalid' : ''; ?>">
                                         <input type="text" name="firstname" <?php if (!empty($errorList)) { echo 'placeholder="' . showErrors('firstname', $errorList) . '"';} ?> required>
-                                        <label for="firstname">Prénom<span class="red_star">*</span></label>
+                                        <label for="firstname">Prénom<span class="yellow_star">*</span></label>
                                     </div>
                                     <div class="inputbox <?= (isset($errorList['email'])) ? 'invalid' : ''; ?>">
                                         <input type="text" name="mail" <?php if (!empty($errorList)) { echo 'placeholder="' . showErrors('email', $errorList) . '"';} ?> required>
-                                        <label for="mail">Email<span class="red_star">*</span></label>
+                                        <label for="mail">Email<span class="yellow_star">*</span></label>
                                     </div>
                                     <div class="inputbox <?= (isset($errorList['message'])) ? 'invalid' : ''; ?>" required>
                                         <textarea name="message" id="message" required></textarea>
-                                        <label for="message">Votre message<span class="red_star">*</span></label>
+                                        <label for="message">Votre message<span class="yellow_star">*</span></label>
                                     </div>
                                     <div class="btn-check-form">
                                         <input  type="checkbox" value="" id="copy" name="copy">
