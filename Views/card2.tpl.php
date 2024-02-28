707fefa29b7bd5c862f6a67313f6878d96ecf5ac
Deunoh <denovann@live.fr>
2024-02-28 14:36:03 +0100

    text presentation

--- Views/card2.tpl.php
+++ Views/card2.tpl.php
@@ -5,8 +5,12 @@
                     </div>
                         <section class="mid-section">
                             <h2 class="title-card">Présentation<span id="input-cursor">|</span></h2>
-                            <p class="glass__paragraphe"> Après une décennie passée dans le secteur de la logistique, j'ai décidé de me réorienter vers l'informatique, un domaine qui me passionne.<br> Suite à un bilan de compétences visant à valider mon choix, j'ai entamé une <strong class="strong_presentation">formation de développeur web et web mobile</strong> au sein de l'école O'clock.<br> Actuellement, je suis à la <strong class="strong_presentation">recherche</strong> d'une opportunité d'<strong class="strong_presentation">alternance</strong> à partir d'octobre 2024 dans le cadre de la formation de <strong class="strong_presentation">Concepteur Développeur d'Applications</strong>, toujours au sein de l'école O'clock.</p>
-                            <div>
+                            <div class="glass__paragraphe"><p>Après une décennie passée dans le secteur de la logistique, j'ai décidé de me réorienter vers l'informatique, un domaine qui me passionne.</p>
+                             <p>Suite à un bilan de compétences visant à valider mon choix, j'ai entamé une <strong class="strong_presentation">formation de développeur web et web mobile</strong> au sein de l'école O'clock.</p>
+                              <p>Actuellement, je suis à la <strong class="strong_presentation">recherche</strong> d'une opportunité d'<strong class="strong_presentation">alternance</strong>
+                              à partir d'octobre 2024 dans le cadre de la formation de <strong class="strong_presentation">Concepteur Développeur d'Applications</strong>, toujours au sein de l'école O'clock.</p>
+                            </div>
+                            <div class="social-list-box">
                                 <ul class="social-list">
                                     <li><a href="mailto:denovann.belloir@oclock.school" target="_blank"><img src="img/email.png" alt="logo email" class="logo-contact"></a></li>
                                     <li><a href="https://github.com/Deunoh" target="_blank"><img src="img/github.png" alt="logo github" class="logo-contact"></a></li>
