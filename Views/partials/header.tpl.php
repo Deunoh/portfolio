2c5814390682d195ad71a14efaab2ae8651f294a
Deunoh <denovann@live.fr>
2024-03-08 15:56:08 +0100

    refesh css mobile

--- Views/partials/header.tpl.php
+++ Views/partials/header.tpl.php
@@ -17,7 +17,7 @@
 
     
     <link rel="stylesheet" href="./css/reset.css">
-    <link rel="stylesheet" media="screen and (max-width: 950px)" href="./css/mobiles.css">
+    <link rel="stylesheet" media="screen and (max-width: 950px)" href="./css/mobile-style.css">
     <link rel="stylesheet" media="screen and (min-width: 951px)" href="./css/desktop.css">
 
 </head>
